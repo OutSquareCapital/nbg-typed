55711e64f84c38384c9354dddec2aae2745fc932
OutSquareCapital <166045166+OutSquareCapital@users.noreply.github.com>
2025-06-11 02:58:51 +0200

    first commit

---
+++ numbagg/__init__.pyi
@@ -0,0 +1,36 @@
+from numbagg.moving import move_corr, move_cov, move_mean, move_std, move_sum, move_var
+from numbagg.moving_exp import (
+    move_exp_nancorr,
+    move_exp_nancov,
+    move_exp_nanmean,
+    move_exp_nanstd,
+    move_exp_nansum,
+    move_exp_nanvar,
+    move_exp_nancount,
+)
+from numbagg.funcs import bfill, ffill, allnan, anynan, nancount, nansum, nanmean, nanvar, nanstd
+
+__all__ = [
+    "move_mean",
+    "move_sum",
+    "move_std",
+    "move_var",
+    "move_cov",
+    "move_corr",
+    "bfill",
+    "ffill",
+    "move_exp_nancount",
+    "move_exp_nanmean",
+    "move_exp_nansum",
+    "move_exp_nanvar",
+    "move_exp_nanstd",
+    "move_exp_nancov",
+    "move_exp_nancorr",
+    "allnan",
+    "anynan",
+    "nancount",
+    "nansum",
+    "nanmean",
+    "nanvar",
+    "nanstd",
+]
---
+++ numbagg/funcs.pyi
@@ -0,0 +1,86 @@
+import numpy as np
+from numpy.typing import NDArray
+from typing import overload
+
+type FloatScalar = np.float64 | np.float32
+type NumericScalar = np.float64 | np.float32 | np.int32 | np.int64
+type NumericArray = (
+    NDArray[np.float64] | NDArray[np.float32] | np.int32 | NDArray[np.int64]
+)
+
+def bfill[T: FloatScalar](
+    arr: NDArray[T],
+    /,
+    *,
+    limit: int | None = None,
+    axis: int = -1,
+) -> NDArray[T]: ...
+def ffill[T: FloatScalar](
+    arr: NDArray[T],
+    /,
+    *,
+    limit: int | None = None,
+    axis: int = -1,
+) -> NDArray[T]: ...
+def allnan(
+    arr: NumericArray,
+    /,
+    *,
+    axis: int,
+) -> NDArray[np.bool_]: ...
+def anynan(
+    arr: NumericArray,
+    /,
+    *,
+    axis: int,
+) -> NDArray[np.bool_]: ...
+def nancount(
+    arr: NumericArray,
+    /,
+    *,
+    axis: int,
+) -> NDArray[np.int64]: ...
+def nansum[T: NumericScalar](
+    arr: NDArray[T],
+    /,
+    *,
+    axis: int,
+) -> NDArray[T]: ...
+def nanmean[T: FloatScalar](
+    arr: NDArray[T],
+    /,
+    *,
+    axis: int,
+) -> NDArray[T]: ...
+def nanvar[T: FloatScalar](
+    arr: NDArray[T],
+    /,
+    *,
+    ddof: int = 1,
+    axis: int,
+) -> NDArray[T]: ...
+def nanstd[T: FloatScalar](
+    arr: NDArray[T],
+    /,
+    *,
+    ddof: int = 1,
+    axis: int,
+) -> NDArray[T]: ...
+def nanargmax(arr: NumericArray, /, *, axis: int) -> NDArray[np.int64]: ...
+def nanargmin(arr: NumericArray, /, *, axis: int) -> NDArray[np.int64]: ...
+@overload
+def nanmax(
+    arr: NDArray[np.int64] | NDArray[np.int32], /, *, axis: int
+) -> NDArray[np.int64]: ...
+@overload
+def nanmax(arr: NDArray[np.float32], /, *, axis: int) -> NDArray[np.float32]: ...
+@overload
+def nanmax(arr: NDArray[np.float64], /, *, axis: int) -> NDArray[np.float64]: ...
+@overload
+def nanmin(
+    arr: NDArray[np.int64] | NDArray[np.int32], /, *, axis: int
+) -> NDArray[np.int64]: ...
+@overload
+def nanmin(arr: NDArray[np.float32], /, *, axis: int) -> NDArray[np.float32]: ...
+@overload
+def nanmin(arr: NDArray[np.float64], /, *, axis: int) -> NDArray[np.float64]: ...
